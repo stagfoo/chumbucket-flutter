56479dbe6ca6b5268e8c0659defb6df8bf4a8dbd
stagfoo <alexanderddking@gmail.com>
2022-12-29 22:54:48 +0900

    🪴　feature ░ setup map

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,36 @@ import 'ui.dart';
 void main() {
   runApp(GetMaterialApp(
     initialRoute: '/',
+    theme: ThemeData(
+      // Define the default brightness and colors.
+      brightness: Brightness.light,
+      primaryColor: Colors.pink[800],
+
+      // Define the default `TextTheme`. Use this to specify the default
+      // text styling for headlines, titles, bodies of text, and more.
+      textTheme: const TextTheme(
+        headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+        bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+      ),
+    ),
     getPages: [
       GetPage(
           name: '/',
-          page: () => const PageOne(),
+          page: () => const HomePage(),
           transition: Transition.fadeIn),
       GetPage(
-          name: '/second',
-          page: () => const PageTwo(),
+          name: '/goals',
+          page: () => const GoalsPage(),
+          transition: Transition.fadeIn),
+      GetPage(
+          name: '/collections',
+          page: () => const CollectionsPage(),
+          transition: Transition.fadeIn),
+      GetPage(
+          name: '/add-marker',
+          page: () => const AddMarkerPage(),
           transition: Transition.fadeIn),
     ],
   ));
 }
-
-
-
--- lib/ui.dart
+++ lib/ui.dart
@@ -1,28 +1,70 @@
 //Libs
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 //Local
 import 'store.dart';
 
-class PageOne extends StatelessWidget {
-  const PageOne({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: ElevatedButton(
-      child: const Text('Page one'),
-      onPressed: () {
-        Get.toNamed("/second");
-      },
-    ),
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(35.689487, 139.691711),
+          onLongPress: (tapPosition, point) => {
+            // ignore: avoid_print
+            print(tapPosition),
+            print(point)
+          },
+          zoom: 14,
+          maxZoom: 18,
+          rotation: 180.0,
+          keepAlive: true,
+        ),
+        nonRotatedChildren: [
+          AttributionWidget.defaultWidget(
+            source: 'OpenStreetMap contributors',
+            onSourceTapped: null,
+          ),
+        ],
+        children: [
+          TileLayer(
+            urlTemplate:
+                'https://api.mapbox.com/styles/v1/stagfoo/ck7na75980hpr1iqnrfpg9ffr/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3RhZ2ZvbyIsImEiOiJjazdsNXZodWIwNDY5M2VvbWF0ejc2M2ptIn0.-DgAxfXGVWow1PEToMyaOg',
+            userAgentPackageName: 'com.example.app',
+            additionalOptions: const {
+              "access_token":
+                  "pk.eyJ1Ijoic3RhZ2ZvbyIsImEiOiJjazdsNXZodWIwNDY5M2VvbWF0ejc2M2ptIn0.-DgAxfXGVWow1PEToMyaOg"
+            },
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(30, 40),
+                width: 80,
+                height: 80,
+                builder: (context) => FlutterLogo(),
+              ),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Get.toNamed("/add-marker");
+        },
+      ),
     );
   }
 }
 
-class PageTwo extends StatelessWidget {
-  const PageTwo({Key? key}) : super(key: key);
+class GoalsPage extends StatelessWidget {
+  const GoalsPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -35,6 +77,41 @@ class PageTwo extends StatelessWidget {
         ),
         onPressed: () {
           GlobalState.to.increment();
+        },
+      ),
+    );
+  }
+}
+
+class CollectionsPage extends StatelessWidget {
+  const CollectionsPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: GetBuilder<GlobalState>(
+          init: GlobalState(),
+          builder: (_) => Text(
+            '${_.counter}',
+          ),
+        ),
+        onPressed: () {
+          GlobalState.to.increment();
+        },
+      ),
+    );
+  }
+}
+
+class AddMarkerPage extends StatelessWidget {
+  const AddMarkerPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text('Go Back'),
+        onPressed: () {
+          Get.toNamed("/");
         },
       ),
     );
