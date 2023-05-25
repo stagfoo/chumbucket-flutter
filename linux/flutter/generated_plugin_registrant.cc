900ae96f2b9b2269304f3be1ae16b4cfb4f954f9
Alex@basal <alex@basal.dev>
2023-05-25 13:28:38 +0900

    🍱　refactor ░ change icons and generations

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,14 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <fast_rsa/fast_rsa_plugin.h>
+#include <openpgp/openpgp_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) fast_rsa_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FastRsaPlugin");
+  fast_rsa_plugin_register_with_registrar(fast_rsa_registrar);
+  g_autoptr(FlPluginRegistrar) openpgp_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "OpenpgpPlugin");
+  openpgp_plugin_register_with_registrar(openpgp_registrar);
 }
